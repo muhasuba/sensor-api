a2db153a7d976d8937700c6a9b7e0af7c3a43b4e
Subair Muhammad <muhammad.subair@solteq.com>
2022-12-18 22:42:23 +0200

    update put for update and delete sample + test

--- app/main/service/sensor_service.py
+++ app/main/service/sensor_service.py
@@ -9,12 +9,25 @@ from typing import Dict, Tuple
 def save_new_sensor(data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
     sensor = Sensor.query.filter_by(title=data['title']).first()
     if not sensor:
-        new_sensor = Sensor(
-            title=data['title'],
-            info=data['info'],
-            last_update=datetime.datetime.utcnow()
-        )
-        return save_changes(new_sensor)
+        try:
+            new_sensor = Sensor(
+                title=data['title'],
+                info=data['info'],
+                last_update=datetime.datetime.utcnow()
+            )
+            db.session.add(new_sensor)
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': 'Successfully created.',
+            }
+            return response_object, 201
+        except Exception as e:
+            response_object = {
+                'status': 'fail',
+                'message': 'Some error occurred. Please try again.'
+            }
+            return response_object, 400
     else:
         response_object = {
             'status': 'fail',
@@ -31,18 +44,44 @@ def get_a_sensor(sensor_id):
     return Sensor.query.filter_by(id=sensor_id).first()
 
 
-def save_changes(data: Sensor) -> None:
+def delete_a_sensor(sensor_id):
     try:
-        db.session.add(data)
+        Sensor.query.filter_by(id=sensor_id).delete()
         db.session.commit()
         response_object = {
             'status': 'success',
-            'message': 'Successfully created.',
+            'message': 'Successfully deleted.',
         }
-        return response_object, 201
+        return response_object, 200
     except Exception as e:
         response_object = {
             'status': 'fail',
             'message': 'Some error occurred. Please try again.'
         }
         return response_object, 400
+
+
+def update_a_sensor(data: Dict[str, str], sensor_to_update: Sensor) -> Tuple[Dict[str, str], int]:
+    sensor = Sensor.query.filter_by(title=data['title']).first()
+    if not sensor:
+        try:
+            sensor_to_update.title=data['title']
+            sensor_to_update.info=data['info']
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': 'Successfully updated.',
+            }
+            return response_object, 200
+        except Exception as e:
+            response_object = {
+                'status': 'fail',
+                'message': 'Some error occurred. Please try again.'
+            }
+            return response_object, 400
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'Sensor already exists.',
+        }
+        return response_object, 409
--- app/test/test_sensor.py
+++ app/test/test_sensor.py
@@ -19,6 +19,25 @@ def get_all_sensors(self):
     return self.client.get('/sensor/')
 
 
+def get_a_sensor(self, sensor_id):
+    return self.client.get(f'/sensor/{sensor_id}')
+
+
+def delete_a_sensor(self, sensor_id):
+    return self.client.delete(f'/sensor/{sensor_id}')
+
+
+def update_a_sensor(self, sensor_id):
+    return self.client.put(
+        f'/sensor/{sensor_id}',
+        data=json.dumps(dict(
+            title='Sensor 1 update',
+            info='Info sensor 1 update'
+        )),
+        content_type='application/json'
+    )
+
+
 class TestSensor(BaseTestCase):
     def test_get_all_sensors_initial(self):
         """ Test get all sensors initial """
@@ -59,6 +78,60 @@ class TestSensor(BaseTestCase):
             self.assertEqual(len(data['data']), 1)
             self.assertEqual(data['data'][0]['title'], "Sensor 1")
             self.assertEqual(response.status_code, 200)
+    
+    def test_get_specific_sensor_after_register(self):
+        """ Test get specific sensor after register """
+        register_sensor(self)
+        with self.client:
+            response = get_a_sensor(self, 1)
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['title'], "Sensor 1")
+            self.assertEqual(response.status_code, 200)
+    
+    def test_failed_get_specific_sensor_after_register(self):
+        """ Test failed get specific sensor not exist """
+        with self.client:
+            response = get_a_sensor(self, 1000)
+            self.assertEqual(response.status_code, 404)
+    
+    def test_delete_sensor_after_register(self):
+        """ Test delete sensor after register """
+        register_sensor(self)
+        with self.client:
+            response = delete_a_sensor(self, 1)
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == 'Successfully deleted.')
+            self.assertEqual(response.status_code, 200)
+    
+    def test_failed_delete_specific_sensor_after_register(self):
+        """ Test failed get specific sensor not exist """
+        register_sensor(self)
+        with self.client:
+            response = delete_a_sensor(self, 1000)
+            self.assertEqual(response.status_code, 404)
+    
+    def test_update_sensor_after_register(self):
+        """ Test update sensor after register """
+        register_sensor(self)
+        with self.client:
+            response_update = update_a_sensor(self, 1)
+            data_update = json.loads(response_update.data.decode())
+            self.assertTrue(data_update['status'] == 'success')
+            self.assertTrue(data_update['message'] == 'Successfully updated.')
+            self.assertEqual(response_update.status_code, 200)
+            response_get = get_a_sensor(self, 1)
+            data_get = json.loads(response_get.data.decode())
+            self.assertEqual(data_get['title'], "Sensor 1 update")
+            self.assertEqual(data_get['info'], "Info sensor 1 update")
+            self.assertEqual(response_get.status_code, 200)
+    
+    def test_failed_update_specific_sensor_after_register(self):
+        """ Test failed get specific sensor not exist """
+        register_sensor(self)
+        with self.client:
+            response = update_a_sensor(self, 1000)
+            self.assertEqual(response.status_code, 404)
 
 
 if __name__ == '__main__':
